811c7b83c120d91f5376826f7b2fa1229af7d89a
Romenildo <romenildoferreira@gmail.com>
2023-12-10 10:31:15 -0300

    [API] Criando as entidades e relacionamentos

---
+++ back/Projeto_Treino/Entities/Entidades/Base.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Notificacoes;
+
+namespace Entities.Entidades
+{
+    // a classe base agora possui todas as caracteristicas do notifica com a lista de mensagens
+    public class Base : Notifica
+    {
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+    }
+}
